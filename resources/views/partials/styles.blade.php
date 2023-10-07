07172a3c05a8b622b0fe04b61a54260be27a5988
Pos3iD1 <pos3id1@gmail.com>
2023-10-07 21:38:00 +0600

    Created partial view for css styles

---
+++ resources/views/partials/styles.blade.php
@@ -0,0 +1,25 @@
+<!-- build:css /assets/css/app.min.css -->
+<!--== Leaflet Min CSS ==-->
+<link href="/assets/css/leaflet.min.css" rel="stylesheet"/>
+<!--== Nice Select Min CSS ==-->
+<link href="/assets/css/nice-select.min.css" rel="stylesheet"/>
+<!--== Slick Slider Min CSS ==-->
+<link href="/assets/css/slick.min.css" rel="stylesheet"/>
+<!--== Magnific Popup Min CSS ==-->
+<link href="/assets/css/magnific-popup.min.css" rel="stylesheet"/>
+<!--== Slicknav Min CSS ==-->
+<link href="/assets/css/slicknav.min.css" rel="stylesheet"/>
+<!--== Animate Min CSS ==-->
+<link href="/assets/css/animate.min.css" rel="stylesheet"/>
+<!--== Ionicons Min CSS ==-->
+<link href="/assets/css/ionicons.min.css" rel="stylesheet"/>
+<!--== Font-Awesome Min CSS ==-->
+<link href="/assets/css/font-awesome.min.css" rel="stylesheet"/>
+<!--== Bootstrap Min CSS ==-->
+<link href="/assets/css/bootstrap.min.css" rel="stylesheet"/>
+
+<!--== Main Style CSS ==-->
+<link href="/assets/css/style.css" rel="stylesheet"/>
+<!--== Helper Min CSS ==-->
+<link href="/assets/css/helper.min.css" rel="stylesheet"/>
+<!-- endbuild -->
