a2eea96a4aea38fe750e852e183d9b9715f02d12
Pos3iD1 <pos3id1@gmail.com>
2023-10-07 21:37:50 +0600

    Created partial view for js scripts

---
+++ resources/views/partials/scripts.blade.php
@@ -0,0 +1,28 @@
+<!--=======================Javascript============================-->
+<!-- build:js /assets/js/app.min.js -->
+<!--=== Modernizr Min Js ===-->
+<script src="/assets/js/modernizr-3.6.0.min.js"></script>
+<!--=== jQuery Min Js ===-->
+<script src="/assets/js/jquery-3.3.1.min.js"></script>
+<!--=== jQuery Migration Min Js ===-->
+<script src="/assets/js/jquery-migrate-1.2.1.min.js"></script>
+<!--=== Popper Min Js ===-->
+<script src="/assets/js/popper.min.js"></script>
+<!--=== Bootstrap Min Js ===-->
+<script src="/assets/js/bootstrap.min.js"></script>
+<!--=== Slicknav Min Js ===-->
+<script src="/assets/js/jquery.slicknav.min.js"></script>
+<!--=== Magnific Popup Min Js ===-->
+<script src="/assets/js/jquery.magnific-popup.min.js"></script>
+<!--=== Slick Slider Min Js ===-->
+<script src="/assets/js/slick.min.js"></script>
+<!--=== Nice Select Min Js ===-->
+<script src="/assets/js/jquery.nice-select.min.js"></script>
+<!--=== Leaflet Min Js ===-->
+<script src="/assets/js/leaflet.min.js"></script>
+<!--=== Countdown Js ===-->
+<script src="/assets/js/countdown.js"></script>
+
+<!--=== Active Js ===-->
+<script src="/assets/js/active.js"></script>
+<!-- endbuild -->
