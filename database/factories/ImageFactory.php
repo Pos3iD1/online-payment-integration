ab3f3fa11ff8a99f4dc5833b12c0a049f3382376
Pos3iD1 <pos3id1@gmail.com>
2023-10-07 21:33:24 +0600

    Created factory for images

---
+++ database/factories/ImageFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Image>
+ */
+class ImageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+//            'product_id',
+            'path' => '/assets/img/product/product-7.png',
+            'alt' => fake()->words(3, true),
+        ];
+    }
+}
