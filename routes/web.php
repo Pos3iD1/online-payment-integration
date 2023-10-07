bc04a78f31665767746094b66734fc29735198ca
Pos3iD1 <pos3id1@gmail.com>
2023-10-07 21:15:13 +0600

    Added routes for new pages

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,48 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', [\App\Http\Controllers\Pages\IndexController::class, 'index']);
+
+Route::get('/about', function () {
+    return view('pages.about');
+});
+
+Route::get('/products', function () {
+    return view('pages.products');
+});
+
+Route::get('/blog', function () {
+    return view('pages.blog');
+});
+
+Route::get('/gallery', function () {
+    return view('pages.gallery');
+});
+
+Route::get('/cart', function () {
+    return view('pages.cart');
+});
+
+Route::get('/checkout', function () {
+    return view('pages.checkout');
+});
+
+Route::get('/wishlist', function () {
+    return view('pages.wishlist');
+});
+
+Route::get('/contact', function () {
+    return view('pages.contact');
+});
+
+Route::get('/register', function () {
+    return view('pages.register');
+});
+
+Route::get('/login', function () {
+    return view('pages.login');
 });
